7536d96679b7d2ed148b68a4881a4644f989f26a
Alex Mah <maxhale4@gmail.com>
2017-08-04 20:19:15 -0400

    Added tank drive to be tested using xbox controller.

--- FRCSummer2017/src/org/usfirst/frc/team5066/robot/OI.java
+++ FRCSummer2017/src/org/usfirst/frc/team5066/robot/OI.java
@@ -1,6 +1,9 @@
 package org.usfirst.frc.team5066.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Joystick.AxisType;
 import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 import org.usfirst.frc.team5066.robot.commands.ExampleCommand;
 
@@ -14,9 +17,43 @@ public class OI {
 	//// joystick.
 	// You create one by telling it which joystick it's on and which button
 	// number it is.
-	// Joystick stick = new Joystick(port);
-	// Button button = new JoystickButton(stick, buttonNumber);
+	Joystick xboxController = new Joystick(0);
+	
+	Button abutton = new JoystickButton(xboxController, 1);
+	Button bButton = new JoystickButton(xboxController, 2);
+	Button xbutton = new JoystickButton(xboxController, 3);
+	Button yButton = new JoystickButton(xboxController, 4);
+	Button lb = new JoystickButton(xboxController, 5);
+	Button rb = new JoystickButton(xboxController, 6);
+	Button back = new JoystickButton(xboxController, 7);
+	Button start = new JoystickButton(xboxController, 8);
+	Button l3 = new JoystickButton(xboxController, 9);
+	Button r3 = new JoystickButton(xboxController, 10);
+	
+	public double getLS_X() {
+		return xboxController.getRawAxis(0);
+	}
+	
+	public double getLS_Y() {
+		return xboxController.getRawAxis(1);
+	}
+	
+	public double getRS_X() {
+		return xboxController.getRawAxis(4);
+	}
+	
+	public double getRS_Y() {
+		return xboxController.getRawAxis(5);
+	}
+	
+	public double getTriggerRight() {
+		return xboxController.getRawAxis(3);
+	}
+	public double getTriggerLeft() {
+		return xboxController.getRawAxis(2);
+	}
 
+	
 	// There are a few additional built in buttons you can use. Additionally,
 	// by subclassing Button you can create custom triggers and bind those to
 	// commands the same as any other Button.
--- FRCSummer2017/src/org/usfirst/frc/team5066/robot/subsystems/Chassis.java
+++ FRCSummer2017/src/org/usfirst/frc/team5066/robot/subsystems/Chassis.java
@@ -2,11 +2,23 @@ package org.usfirst.frc.team5066.robot.subsystems;
 
 import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.command.Subsystem;
-import org.usfirst.frc.team5066.robot.RobotMap;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+import org.usfirst.frc.team5066.robot.subsystems.SpeedMode;
+import org.usfirst.frc.team5066.robot.RobotMap;
 import com.ctre.CANTalon;
 
 public class Chassis extends Subsystem{
+	
+	private double DEFAULT_MINIMUM_THRESHOLD = 0.09;
+	private double velocityMultiplier = 1.0;
+	private final static double DEFAULT_SLOW_SPEED_CONSTANT = 0.5;
+	private final static double DEFAULT_NORMAL_SPEED_CONSTANT = 0.8;
+	private final static double DEFAULT_FAST_SPEED_CONSTANT = 1.0;
+	
+	double slowSpeedConstant = DEFAULT_SLOW_SPEED_CONSTANT;
+	double normalSpeedConstant = DEFAULT_NORMAL_SPEED_CONSTANT;
+	double fastSpeedConstant = DEFAULT_FAST_SPEED_CONSTANT;
 	
 	SpeedController frontLeftMotor = RobotMap.driveFrontLeftMotor;
 	SpeedController middleLeftMotor = RobotMap.driveMiddleLeftMotor;
@@ -15,10 +27,18 @@ public class Chassis extends Subsystem{
 	SpeedController middleRightMotor = RobotMap.driveMiddleRightMotor;
 	SpeedController rearRightMotor = RobotMap.driveRearRightMotor;
 
+		
 	@Override
 	protected void initDefaultCommand() {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	private double threshold(double velocity) {
+		if (Math.abs(velocity) <= DEFAULT_MINIMUM_THRESHOLD) {
+			return 0;
+		}
+		return velocity;
 	}
 	
 	public void driveForward(){
@@ -30,8 +50,93 @@ public class Chassis extends Subsystem{
 		rearRightMotor.set(0.5);
 	}
 	
-	public void stop(){
+	public void turnFLMotor(double speed){
+		frontLeftMotor.set(speed);
+	}
+	
+	public void turnMLMotor(double speed){
+		middleLeftMotor.set(speed);
+	}
+	
+	public void turnRLMotor(double speed){
+		rearLeftMotor.set(speed);
+	}
+	
+	public void turnFRMotor(double speed){
+		frontRightMotor.set(speed);
+	}
+	
+	public void turnMRMotor(double speed){
+		middleRightMotor.set(speed);
+	}
+	
+	public void turnRRMotor(double speed){
+		rearRightMotor.set(speed);
+	}
+	
+	public void arcadeSixWheel(double translation, double rotation, boolean squaredInputs, SpeedMode speedMode) {
+		double translationVelocity = translation, rotationVelocity = rotation;
 		
+		translationVelocity = threshold(translationVelocity);
+		rotationVelocity = threshold(rotationVelocity);
+		
+		setVelocityMultiplierBasedOnSpeedMode(speedMode);
+		
+		// Do squared inputs if necessary
+		if (squaredInputs) {
+			translationVelocity *= Math.abs(translation);
+			rotationVelocity *= Math.abs(rotation);
+		}
+		
+		// Do reverse drive when necessary. There are methods above for different inputs.
+		/*if (reverse) {
+			translationVelocity = -translationVelocity;
+			rotationVelocity = -rotationVelocity;
+		}
+		*/
+		// Guard against illegal values
+		double maximum = Math.max(1, Math.abs(translationVelocity) + Math.abs(rotationVelocity));
+
+		
+		translationVelocity = threshold(translationVelocity);
+		rotationVelocity = threshold(rotationVelocity);
+
+		// Set the motors
+		frontLeftMotor.set(this.velocityMultiplier * ((-translationVelocity + rotationVelocity) / maximum));
+		rearLeftMotor.set(this.velocityMultiplier * ((-translationVelocity + rotationVelocity) / maximum));
+		middleLeftMotor.set(this.velocityMultiplier * ((-translationVelocity + rotationVelocity) / maximum));
+		middleRightMotor.set(this.velocityMultiplier * ((translationVelocity + rotationVelocity) / maximum));
+		frontRightMotor.set(this.velocityMultiplier * ((translationVelocity + rotationVelocity) / maximum));
+		rearRightMotor.set(this.velocityMultiplier * ((translationVelocity + rotationVelocity) / maximum));
+		//SmartDashboard.putNumber("rightEncoder", ((CANTalon) m_rightMiddleMotor).getEncPosition());
+		//SmartDashboard.putNumber("leftEncoder", ((CANTalon) m_leftMiddleMotor).getEncPosition());
+	}
+	
+private void setVelocityMultiplierBasedOnSpeedMode(SpeedMode speedMode) {
+		
+		switch(speedMode) {
+		case SLOW:
+			velocityMultiplier = this.slowSpeedConstant;
+			SmartDashboard.putString("DB/String 8", "Using slow speed constant");
+			break;
+		case NORMAL:
+			velocityMultiplier = this.normalSpeedConstant;
+			SmartDashboard.putString("DB/String 8", "Using normal speed constant");
+			break;
+		case FAST:
+			velocityMultiplier = this.fastSpeedConstant;
+			SmartDashboard.putString("DB/String 8", "Using fast speed constant");
+			break;
+		}
+	}
+	
+	public void stop(){
+		frontLeftMotor.set(0);
+		middleLeftMotor.set(0);
+		rearLeftMotor.set(0);
+		frontRightMotor.set(0);
+		middleRightMotor.set(0);
+		rearRightMotor.set(0);
 	}
 
 }
